770d472b6357a2391bdd02744434b8d398145f70
Alex Botelho <alexbotelho1@hotmail.com>
2020-07-22 16:39:49 -0300

    Tela de Login ok

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>Hello World</h1>
+import GlobalStyle from './styles/global';
+import SignIn from './pages/SignIn';
+
+const App: React.FC = () => (
+    <>
+        <GlobalStyle />
+        <SignIn />
+    </>
+);
 
 export default App;
