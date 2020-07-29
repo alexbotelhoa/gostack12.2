e87e04d2dd6ca0d741c16fdd1bda3a13e0b380b1
Alex Botelho <alexbotelho1@hotmail.com>
2020-07-28 22:12:43 -0300

    Login pelo Contexto

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,8 +1,10 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+
+import AuthContext from '../../context/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import { Background, Container, Content } from './styles';
@@ -14,6 +16,8 @@ import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
+
+    const auth = useContext(AuthContext);
 
     const handleSubmit = useCallback( async (data: object) => {
         try {
